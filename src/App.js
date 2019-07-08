07a982c43b48b7f0986166398db064e72e417076
Clark Williams <theclarkwilliams@gmail.com>
2019-07-08 13:34:09 -0600

    added extra point button, changed touchdown to 6 points, added clear scores button

--- src/App.js
+++ src/App.js
@@ -31,23 +31,41 @@ function App() {
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button 
             className="homeButtons__touchdown"
-            onClick={() => {setHomeScore(homeScore + 7)}}
-          >Home Touchdown</button>
+            onClick={() => {setHomeScore(homeScore + 6)}}
+          >Home Touchdown
+          </button>
           <button 
             className="homeButtons__fieldGoal"
             onClick={() => {setHomeScore(homeScore + 3)}}
-          >Home Field Goal</button>
+          >Home Field Goal
+          </button>
+          <button 
+            className="homeButtons_extraPoint"
+            onClick={() => {setHomeScore(homeScore + 1)}}
+          >Home Extra Point
+          </button>
         </div>
         <div className="awayButtons">
           <button 
             className="awayButtons__touchdown"
-            onClick={() => {setAwayScore(awayScore + 7)}}
-          >Away Touchdown</button>
+            onClick={() => {setAwayScore(awayScore + 6)}}
+          >Away Touchdown
+          </button>
           <button 
             className="awayButtons__fieldGoal"
             onClick={() => {setAwayScore(awayScore + 3)}}
-          >Away Field Goal</button>
+          >Away Field Goal
+          </button>
+          <button 
+            className="awayButtons_extraPoint"
+            onClick={() => {setAwayScore(awayScore + 1)}}
+          >Away Extra Point
+          </button>
         </div>
+        <button onClick={() => {
+          setHomeScore(homeScore * 0);
+          setAwayScore(awayScore * 0)
+          }} >Clear Scores</button>
       </section>
     </div>
   );
